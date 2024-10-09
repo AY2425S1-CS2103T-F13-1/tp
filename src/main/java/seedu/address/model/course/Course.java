d61a828d9361e6342340779d0fc125577046b1d8
Marcus Ang <marcaroni28@gmail.com>
2024-10-09 15:53:23 +0800

    edit gradle files

--- src/main/java/seedu/address/model/course/Course.java
+++ src/main/java/seedu/address/model/course/Course.java
@@ -1,6 +1,7 @@
 package seedu.address.model.course;
 
 import static java.util.Objects.requireNonNull;
+
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
@@ -9,13 +10,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Course {
 
-<<<<<<< HEAD
     public static final String MESSAGE_CONSTRAINTS = "Courses should be in the format of two letters "
         + "followed by four digits, e.g., CS2103T.";
-=======
-    public static final String MESSAGE_CONSTRAINTS = "Courses should be in the format of two letters followed by four digits, e.g., CS2103T.";
-    
->>>>>>> parent of ec0de729 (edit tests)
     /*
      * The course code must follow the format of two letters followed by four digits.
      * It can optionally have a trailing letter. E.g., "CS2103T", "cs2100".
