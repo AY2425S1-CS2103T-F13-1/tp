70498beec9c0e071b3c8b0537510f6a805fee5a7
yhanyi <yeohhanyi0916@gmail.com>
2024-10-14 12:33:13 +0800

    Update CARL to build with courses

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -32,7 +32,8 @@ public class TypicalPersons {
             .withEmail("johnd@example.com").withPhone("98765432")
             .withTags("owesMoney", "friends").build();
     public static final Student CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").withAddress("wall street").build();
+            .withEmail("heinz@example.com").withAddress("wall street")
+            .withCourses("CS2103T").build();
     public static final Student DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
             .withEmail("cornelia@example.com").withAddress("10th street").withTags("friends").build();
     public static final Student ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
